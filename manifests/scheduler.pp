4b3707d8a608ab56c57d9c146df0f28fb182fc93
Dan Norris <daniel.norris@acquia.com>
2015-06-16 16:28:55 -0400

    Get skeleton set up for scheduler spec

--- manifests/scheduler.pp
+++ manifests/scheduler.pp
@@ -10,13 +10,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class aurora::scheduler(
+class aurora::scheduler (
   $version = $aurora::version,
   $ensure = $aurora::ensure,
   $owner = $aurora::owner,
   $group = $aurora::group,
   $master = $aurora::master,
+  $manage_package = $aurora::manage_package,
+  $libmesos_log_verbosity = $aurora::libmesos_log_verbosity,
+  $libprocess_port = $aurora::libprocess_port,
+  $java_opts = $aurora::java_opts,
+  $cluster_name = $aurora::cluster_name,
+  $http_port = $aurora::http_port,
+  $quorum_size = $aurora::quorum_size,
+  $zookeeper = $aurora::zookeeper,
+  $zookeeper_mesos_path = $aurora::zookeeper_mesos_path,
+  $zookeeper_aurora_path = $aurora::zookeeper_aurora_path,
+  $thermos_executor = $aurora::thermos_executor,
+  $thermos_executor_resources = $aurora::thermos_executor_resources,
+  $thermos_executor_flags = $aurora::thermos_executor_flags,
+  $allowed_container_types = $aurora::allowed_container_types,
+  $gc_executor = $aurora::gc_executor,
+  $log_level = $aurora::log_level,
+  $extra_scheduler_args = $aurora::extra_scheduler_args,
+
 ){
+  include aurora::repo
+
   $aurora_ensure = $version? {
     undef    => 'absent',
     default => $version,
@@ -28,51 +48,47 @@ class aurora::scheduler(
     'aurora-tools',
   ]
 
-  if $aurora::manage_package {
+  if $manage_package {
     package { $packages:
       ensure  => $ensure,
       require => Class['aurora::repo'],
     }
     $aurora_require = Package['aurora-scheduler']
-  }
-  else {
-    $aurora_require = undef
-  }
-
-  file { '/var/lib/aurora/scheduler':
-    ensure  => directory,
-    owner   => $owner,
-    group   => $group,
-    mode    => '0644',
-    require => Package['aurora-scheduler'],
-  }
-
-  file { '/var/lib/aurora/scheduler/db':
-    ensure  => directory,
-    owner   => $owner,
-    group   => $group,
-    mode    => '0644',
-    require => [
-      Package['aurora-scheduler'],
-      File['/var/lib/aurora/scheduler'],
-    ]
-  }
-
-  file { '/etc/default/aurora-scheduler':
-    ensure  => present,
-    content => template('aurora/aurora-scheduler.erb'),
-    owner   => $owner,
-    group   => $group,
-    mode    => '0644',
-    require => $aurora_require
-  }
-
-  if $master {
-    exec { 'init-mesos-log':
-      command => '/usr/bin/mesos-log initialize --path=/var/lib/aurora/scheduler/db && /bin/chown aurora:aurora /var/lib/aurora/scheduler/db/*',
-      unless  => '/usr/bin/test -f /var/lib/aurora/scheduler/db/CURRENT',
+    file { '/var/lib/aurora/scheduler':
+      ensure  => directory,
+      owner   => $owner,
+      group   => $group,
+      mode    => '0644',
       require => Package['aurora-scheduler'],
-      notify  => Service['aurora-scheduler'],
+    }
+
+    file { '/var/lib/aurora/scheduler/db':
+      ensure  => directory,
+      owner   => $owner,
+      group   => $group,
+      mode    => '0644',
+      require => [
+        Package['aurora-scheduler'],
+        File['/var/lib/aurora/scheduler'],
+      ]
+    }
+
+    file { '/etc/default/aurora-scheduler':
+      ensure  => present,
+      content => template('aurora/aurora-scheduler.erb'),
+      owner   => $owner,
+      group   => $group,
+      mode    => '0644',
+      require => $aurora_require
+    }
+
+    if $master {
+      exec { 'init-mesos-log':
+        command => '/usr/bin/mesos-log initialize --path=/var/lib/aurora/scheduler/db && /bin/chown aurora:aurora /var/lib/aurora/scheduler/db/*',
+        unless  => '/usr/bin/test -f /var/lib/aurora/scheduler/db/CURRENT',
+        require => Package['aurora-scheduler'],
+        notify  => Service['aurora-scheduler'],
+      }
     }
   }
 }
--- spec/classes/scheduler_spec.rb
+++ spec/classes/scheduler_spec.rb
@@ -5,69 +5,23 @@ require 'spec_helper'
 $default_package = ['aurora-doc','aurora-scheduler','aurora-tools',]
 #$default_scheduler_file_attr =
 
-describe 'autofs::scheduler' do
-  let (:facts) { { :osfamily => 'Debian', 'aurora_ensure' => '0.9.0' } } # should aurora_ensure be here, or tested
+describe 'aurora::scheduler' do
+  on_supported_os.each do |os, facts|
 
-  it { should contain_file('/var/lib/aurora/scheduler/db').with(:ensure => directory).with_params(
-    :owner => 'aurora',
-    :group => 'aurora',
-    :mode => '0644',
-    :require => [Package['aurora-scheduler'],
-    File['/var/lib/aurora/scehduler']
-  ]
-  ) }
-
-  it { should contain_file('/var/lib/aurora/scheduler').with(:ensure => directory).with_params(
-    :owner => 'aurora',
-    :group => 'aurora',
-    :mode => '0644',
-    :require => Package['aurora-scheduler']
-    ) }
-
-    it { should contain_file('/etc/default/aurora-scheduler').with(:ensure => directory).with_content(:source => 'puppet:///aurora/aurora-scheduler.erb').with_params(
-      :owner => 'aurora',
-      :group => 'aurora',
-      :mode => '0644',
-      :require => Package['aurora-scheduler']
-      ) }
-
-
-  describe ' by default' do
-    let (:facts) { { :manage_package => true } }
-    it { should contain_package('default_package').with(:ensure => '0.9.0').with_params(:require => Class['aurora::repo'] ) }
-    it { should contain_package('aurora-scheduler') }
-
-    context 'with aurora_ensure = default'
-    let (:facts) { { 'aurora_ensure' => '0.9.0'} }
-    it  do
-       should contain_aurora__version('0.9.0') # not sure that this is the right way to write this
-    end
-
-    context 'with aurora_ensure = undef'
-    let (:facts) { { 'aurora_ensure' => 'absent'} }
-    it  do
-       should_not contain_aurora__version('0.9.0') #check syntax
-       should_not compile
-     end
-
-        context 'with $aurora::master => true'
-        let (:facts) { { 'aurora::master' => true } }
-        it { should contain_exec('init-mesos-log').with_params({
-          :require =>  Package['aurora-scheduler'],
-          :notify => Service['aurora-scheduler'],
-           command => '/usr/bin/mesos-log initialize --path=/var/lib/aurora/scheduler/db && /bin/chown aurora:aurora /var/lib/aurora/scheduler/db/*',
-           :unless => '/usr/bin/test -f /var/lib/aurora/scheduler/db/CURRENT' }) }
-
-        context 'with $aurora::master => false'
-        let (:facts) { { 'aurora::master' => false} }
-        it { should_not contain_exec('init-mesos-log').with_params({
-          :require =>  Package['aurora-scheduler'],
-          :notify => Service['aurora-scheduler'],
-          command => '/usr/bin/mesos-log initialize --path=/var/lib/aurora/scheduler/db && /bin/chown aurora:aurora /var/lib/aurora/scheduler/db/*',
-          :unless => '/usr/bin/test -f /var/lib/aurora/scheduler/db/CURRENT'} ) }
-
+    context "on #{os}" do
+      let(:facts) do
+        facts
       end
+
+      let(:params) do
+        {
+          :java_opts => [],
+          :allowed_container_types => ['DOCKER', 'MESOS'],
+          :extra_scheduler_args => [],
+          :manage_package => true,
+        }
+      end
+      it { should compile.with_all_deps }
     end
-
-
-    #should we include context when aurora::master => true in default case, or just its own context/test file?
+  end
+end
